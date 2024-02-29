12bf680d6c7e9b28879e3911691141b29f3c91b3
EvgeniMalev <126523424+EvgeniMalev@users.noreply.github.com>
2024-02-29 23:51:51 +0200

    Create script.js

---
+++ Projects/Travel agency/script.js
@@ -0,0 +1,9 @@
+document.getElementById('text1').addEventListener('click', function() {
+    document.getElementById('text-content').style.display = 'none';
+    document.getElementById('image-content').style.display = 'block';
+});
+
+document.getElementById('text2').addEventListener('click', function() {
+    document.getElementById('text-content').style.display = 'none';
+    document.getElementById('image-content').style.display = 'block';
+});
