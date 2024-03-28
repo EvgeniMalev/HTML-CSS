d0fb814e24bda40a33d75441d5d0c565c9219245
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-28 23:34:11 +0200

    Create Printcombination posititons.php

---
+++ php/Printcombination posititons.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>PHP page</h1>
+
+<?php
+for ($x = 0; $x <= 2; $x++) {
+ for  ($y = 0; $y <= 2; $y++) {
+  echo "The number is: $x$y <br>";
+ }
+}
+?>
+
+</body>
+</html>
