fa1636c90a1585d17d1fdf95b66d4b0afd72630c
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-28 23:26:01 +0200

    Create Printletters.php

---
+++ php/Printletters.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>PHP page</h1>
+
+<?php
+echo "A &nbsp;&nbsp;&nbsp;A &nbsp;&nbsp;&nbsp;A &nbsp;&nbsp;&nbsp;A<br>"; 
+echo "&nbsp;&nbsp;&nbsp;B&nbsp;&nbsp;&nbsp;B&nbsp;&nbsp;&nbsp;B";
+?> 
+
+</body>
+</html>
