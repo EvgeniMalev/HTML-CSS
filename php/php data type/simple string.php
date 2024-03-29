1bee5ede3967cf06e25060a0703d80828fc46b61
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-29 12:40:20 +0200

    Create simple string.php

---
+++ php/php data type/simple string.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$x = "Intel Corei3";
+$y = '13100';
+$y1 = '13100T';
+$y2 = '13100F';
+$m = "Intel Corei5"; 
+$n = '13400';        
+$m1 = '13100T';     
+$m2 = '13100F';      
+$n1 = '13400T';      
+$n2 = '13400F';      
+
+var_dump($x);
+echo "<br>"; 
+var_dump($y);
+var_dump($m);
+echo "<br>"; 
+var_dump($n);
+?>
+
+</body>
+</html>
