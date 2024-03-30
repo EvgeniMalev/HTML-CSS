f7566e9f3e7637e401fcae5f06d55c31f5e04850
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-30 07:45:07 +0200

    Create misedarray.php

---
+++ php/arrays/misedarray.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$array = array(
+    "one" => "first",
+    "two" => "second",
+    "three" => "third",
+      1 => -2,
+      8  => 12,
+);
+var_dump($array);
+?>
+
+</body>
+</html>
