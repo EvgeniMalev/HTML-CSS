fedacd7665ae95339cf9eee26ad98e3e680ec5a8
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-30 08:19:59 +0200

    Create Math roots.php

---
+++ php/Math/Math roots.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 343;
+$y = 16;
+$z = 81;
+$y1 = sqrt($y);
+$x1 = pow($x, 1/3);
+$z1 = pow($z, 1/4);
+
+echo "Cube root of $x is $x1.<br>";
+echo $y1 . "<br>";
+echo $z1 . "<br>";
+?>
+
+</body>
+</html>
